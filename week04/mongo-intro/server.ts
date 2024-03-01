c0efdc0775226ae11ce66a74c811ed67bc8d9502
THCSC <tobias@skancode.dk>
2024-03-01 23:13:09 +0000

    week4 mongo introduction

---
+++ week04/mongo-intro/server.ts
@@ -0,0 +1,39 @@
+import { warn } from 'console';
+import {MongoClient, ObjectId, ServerApiVersion} from 'mongodb'
+
+const uri = "mongodb+srv://tobiasthedanish:9P5RzooWkzwLuzxd@fullstack-ts.kmnlgvk.mongodb.net/"
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const collection = client.db("sample_weatherdata").collection("data");
+
+        // const res = await collection.findOne({_id: new ObjectId("5553a998e4b02cf7151190b8")});
+        // console.log(res);
+
+        // await collection.insertOne({ name: "Sander", age: 25 })
+        // .then(async data => {
+        //     await collection.findOne(data.insertedId).then(data => console.log(data));
+        // });
+
+        // await collection.updateOne({_id: new ObjectId("65d4805d8036f1b1ef2ad7df")}, {$set: { name: "Snadering", age: 22, nose: "HUGE" }})
+        // .then((data) => console.log(data));
+        //
+        // const res = await collection.findOne({_id: new ObjectId("65d4805d8036f1b1ef2ad7df")});
+        // console.log(res?._id.toString());
+
+        await collection.find({"airTemperature.value": {$lt: 0}}).toArray().then(d => console.log(d));
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+run().then(() => console.log("DONE"));
